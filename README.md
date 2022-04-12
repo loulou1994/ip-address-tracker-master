00a7efcf449074ae718da5d75ccd4e59624f8cde
theFrontCoder <le.gas.du43@gmail.com>
2022-04-12 20:11:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![Desktop] (./Desktop.png)
+![Mobile] (./Mobile.jpg)
+
+### Links
+
+- Solution URL: [GitHub Repo](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Postcss Script
+- Vanilla Js
+- LeafLetJs APi
+- GEOIP API
+
+### What I learned
+This is my first ever Js project to include an external API. I learned through the coding process how to interact with the behaviour of an API. I dived a little deep into the API docs to get to know some important concepts such as options inheritance among the API classes.
+
+### Continued development
+I plan on getting myself more aquainted with the use of the different API's out there as they contribute a lot in the performance of any kind of project.
+
+### Useful resources
+
+- (https://www.stackoverflow.com) - This helped me in how to use certain leafletJs option keys.
+- (https://www.github.com) - Helped me solve some misuses I had with the API by checking its github's issues section.
+
+## Author
+- Frontend Mentor - [@loulou1994](https://www.frontendmentor.io/profile/loulou1994)
---
+++ dist/script/app.js
@@ -0,0 +1,138 @@
+const inputBtn = document.querySelector("#input-btn");
+const inputField = document.querySelector("#ip-adress");
+const detailsElements = document.querySelectorAll("[data-id]");
+
+const ipAdresPattern = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$/;
+const apiKey = "at_Ou77WibxoWXFElRSqdbqvggeSTGWq";
+const apiUrl = "https://geo.ipify.org/api/v1?";
+
+// setting a media query object 
+const queryMedia = window.matchMedia("(max-width: 44.9em)");
+
+inputField.addEventListener("keydown", (e) => {
+  const value = e.currentTarget.value;
+  if (e.keyCode == 13) {
+    e.preventDefault();
+    checkIp(value) && processIpAddress(value);
+  }
+});
+
+inputBtn.addEventListener("click", () => {
+  const value = inputField.value;
+  checkIp(value) && getAddressdata(value);
+});
+
+function checkIp(ipAddress) {
+  inputField.value = "";
+  if (ipAddress.match(ipAdresPattern)) return ipAddress;
+  else alert("Invalid Ip Address. Please try again");
+}
+
+function queryHandler(location) {
+    const offseYCordinate = latLngConverter(location);
+  if (queryMedia.matches) {
+      map.setView(offseYCordinate, 13);
+    } else {
+      map.setView(location, 13);
+  }
+}
+
+function latLngConverter(location){
+    let pixelCordinate = map.project(location, 13);
+    pixelCordinate.y -= 100;
+    const latLng = map.unproject(pixelCordinate, 13);
+    return [latLng.lat, latLng.lng]
+}
+
+queryMedia.addEventListener('change', e => queryHandler(latLngPos));
+
+processIpAddress();
+
+
+let map;
+let markerIcon;
+let myMark;
+let latLngPos;
+
+function init(location, city) {
+    map = L.map("map");
+    queryHandler(location);
+    markerIcon = L.icon({
+    iconUrl: "dist/images/icon-location.png",
+    shadowUrl: "dist/images/icon-location3.png",
+
+    iconSize: [30, 45],
+    shadowSize: [38, 75],
+    iconAnchor: [20, 70],
+    shadowAnchor: [17, 70],
+    popupAnchor: [-5, -58],
+  });
+  myMark = L.marker(location, { icon: markerIcon }).addTo(map);
+
+  map.zoomControl.setPosition("bottomright");
+  myMark
+    .bindPopup(
+      `<p style="text-align: center;">Current Location</br><span><b>${city}</b></span></p>`
+    )
+    .openPopup();
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      toolbarAnchor: [0, 0],
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1IjoibG91bG91OTIwOSIsImEiOiJjbDFiZ2huY3IwODF1M2NsNXF5eDd4YXRsIn0.Z1hLqP6WuK-0A2I8ibmYnw",
+    }
+  ).addTo(map);
+}
+
+async function getAddressdata(ipAddress) {
+  const requestUrl = ipAddress
+    ? `${apiUrl}apiKey=${apiKey}&ipAddress=${ipAddress}`
+    : `${apiUrl}apiKey=${apiKey}`;
+  const response = await fetch(requestUrl);
+
+  if (!response.ok) {
+    throw new Error("The request operation failed!");
+  }
+  return response.json();
+}
+
+async function processIpAddress(ipAddress) {
+  let addressData;
+  try {
+    addressData = await getAddressdata(ipAddress);
+  } catch (err) {
+    return console.log("An error occured: " + err);
+  }
+
+  const latlng = [addressData.location.lat, addressData.location.lng];
+  latLngPos = latlng;
+  
+  detailsElements.forEach((element) => {
+    if (element.dataset.id === "timezone") {
+      element.textContent = addressData.location[element.dataset.id];
+    } else if (element.dataset.id === "location") {
+      element.textContent = `${addressData.location.city}, ${addressData.location.region} ${addressData.location.postalCode}`;
+    } else {
+      element.textContent = addressData[element.dataset.id];
+    }
+  });
+
+  if (!ipAddress) {
+    return init(latlng, addressData.location.city);
+  }
+
+  const latLngOffsetY = latLngConverter(latlng);
+  map.flyTo(latLngOffsetY);
+  myMark.setLatLng(latlng);
+  myMark.bindPopup(
+      `<p style="text-align: center;">Current Location</br><span><b>${addressData.location.city}</b></span></p>`
+  );
+}
